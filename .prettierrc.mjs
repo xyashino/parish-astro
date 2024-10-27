24f4fee5b66de66a78c02efdbcb2070af143338d
Szczepan Micek <micek.szczepan.w@gmail.com>
2024-10-27 19:35:09 +0100

    prettier: instal prettier-plugin-tailwindcss

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,22 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
-      },
-    },
+        parser: 'astro'
+      }
+    }
   ],
-};
+  semi: false,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  singleQuote: true,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  printWidth: 80,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  tabWidth: 2
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,129 +1,128 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-  darkMode: "class",
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     fontFamily: {
-      sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      serif: ["Newsreader", ...defaultTheme.fontFamily.serif],
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      serif: ['Newsreader', ...defaultTheme.fontFamily.serif]
     },
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
         },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
         chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
-          3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
-        },
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))'
+        }
       },
       textColor: {
-        main: "hsl(var(--foreground))",
+        main: 'hsl(var(--foreground))'
       },
       backgroundColor: {
-        main: "hsl(var(--background))",
-        muted: "hsl(var(--muted))",
+        main: 'hsl(var(--background))',
+        muted: 'hsl(var(--muted))'
       },
       borderColor: {
-        main: "hsl(var(--border))",
+        main: 'hsl(var(--border))'
       },
-      typography: (theme) => ({
+      typography: theme => ({
         dante: {
           css: {
-            "--tw-prose-body": theme("colors.foreground"),
-            "--tw-prose-headings": theme("colors.foreground"),
-            "--tw-prose-lead": theme("colors.foreground"),
-            "--tw-prose-links": theme("colors.foreground"),
-            "--tw-prose-bold": theme("colors.foreground"),
-            "--tw-prose-counters": theme("colors.foreground"),
-            "--tw-prose-bullets": theme("colors.foreground"),
-            "--tw-prose-hr": theme("colors.border"),
-            "--tw-prose-quotes": theme("colors.foreground"),
-            "--tw-prose-quote-borders": theme("colors.border"),
-            "--tw-prose-captions": theme("colors.foreground"),
-            "--tw-prose-code": theme("colors.foreground"),
-            "--tw-prose-pre-code": theme("colors.zinc.100"),
-            "--tw-prose-pre-bg": theme("colors.zinc.800"),
-            "--tw-prose-th-borders": theme("colors.border"),
-            "--tw-prose-td-borders": theme("colors.border"),
-          },
+            '--tw-prose-body': theme('colors.foreground'),
+            '--tw-prose-headings': theme('colors.foreground'),
+            '--tw-prose-lead': theme('colors.foreground'),
+            '--tw-prose-links': theme('colors.foreground'),
+            '--tw-prose-bold': theme('colors.foreground'),
+            '--tw-prose-counters': theme('colors.foreground'),
+            '--tw-prose-bullets': theme('colors.foreground'),
+            '--tw-prose-hr': theme('colors.border'),
+            '--tw-prose-quotes': theme('colors.foreground'),
+            '--tw-prose-quote-borders': theme('colors.border'),
+            '--tw-prose-captions': theme('colors.foreground'),
+            '--tw-prose-code': theme('colors.foreground'),
+            '--tw-prose-pre-code': theme('colors.zinc.100'),
+            '--tw-prose-pre-bg': theme('colors.zinc.800'),
+            '--tw-prose-th-borders': theme('colors.border'),
+            '--tw-prose-td-borders': theme('colors.border')
+          }
         },
         DEFAULT: {
           css: {
             a: {
-              fontWeight: "normal",
-              textDecoration: "underline",
-              textDecorationStyle: "dashed",
-              textDecorationThickness: "1px",
-              textUnderlineOffset: "2px",
-              "&:hover": {
-                textDecorationStyle: "solid",
-              },
+              fontWeight: 'normal',
+              textDecoration: 'underline',
+              textDecorationStyle: 'dashed',
+              textDecorationThickness: '1px',
+              textUnderlineOffset: '2px',
+              '&:hover': {
+                textDecorationStyle: 'solid'
+              }
             },
-            "h1,h2,h3,h4,h5,h6": {
-              fontFamily: theme("fontFamily.serif"),
-              fontWeight: 500,
+            'h1,h2,h3,h4,h5,h6': {
+              fontFamily: theme('fontFamily.serif'),
+              fontWeight: 500
             },
             blockquote: {
               border: 0,
-              fontFamily: theme("fontFamily.serif"),
-              fontSize: "1.3125em",
-              fontStyle: "italic",
-              fontWeight: "normal",
+              fontFamily: theme('fontFamily.serif'),
+              fontSize: '1.3125em',
+              fontStyle: 'italic',
+              fontWeight: 'normal',
               lineHeight: 1.4,
               paddingLeft: 0,
               '@media (min-width: theme("screens.sm"))': {
-                fontSize: "1.66667em",
-                lineHeight: 1.3,
-              },
-            },
-          },
+                fontSize: '1.66667em',
+                lineHeight: 1.3
+              }
+            }
+          }
         },
         lg: {
           css: {
             blockquote: {
-              paddingLeft: 0,
-            },
-          },
-        },
-      }),
-    },
+              paddingLeft: 0
+            }
+          }
+        }
+      })
+    }
   },
-  plugins: [require("@tailwindcss/typography"),require("tailwindcss-animate"),
-  ],
-};
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')]
+}
