d02564983cd2c61e6567b3b56b6f4155479fb175
Szczepan Micek <micek.szczepan.w@gmail.com>
2024-10-27 19:33:54 +0100

    csm: create navigation collection

--- tina/config.ts
+++ tina/config.ts
@@ -1,31 +1,29 @@
-import { defineConfig } from "tinacms";
-import schema from "./schema";
+import { defineConfig } from 'tinacms'
+import { navigationCollection } from './collections'
 
-// Your hosting provider likely exposes this as an environment variable
 const branch =
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  "main";
+  'main'
 
 export default defineConfig({
   branch,
 
-  // Get this from tina.io
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
   token: process.env.TINA_TOKEN,
 
   build: {
-    outputFolder: "admin",
-    publicFolder: "public",
+    outputFolder: 'admin',
+    publicFolder: 'public'
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "public",
-    },
+      mediaRoot: '',
+      publicFolder: 'public'
+    }
   },
-  // Use the schema imported from schema.ts
-  schema,
-});
+  schema: {
+    collections: [navigationCollection]
+  }
+})
