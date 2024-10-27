0e5a3be9286874a8a1a99d872989a090560dd4c8
Szczepan Micek <micek.szczepan.w@gmail.com>
2024-10-27 13:49:44 +0100

    chore: load template

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx(), sitemap()],
 });
