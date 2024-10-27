68353993e77304a5c35e6ff3a736875cc0c93e1d
Szczepan Micek <micek.szczepan.w@gmail.com>
2024-10-27 10:57:36 +0100

    chore: add prettier

--- tina/config.ts
+++ tina/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "tinacms";
+import schema from "./schema";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
@@ -25,29 +26,6 @@ export default defineConfig({
       publicFolder: "public",
     },
   },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+  // Use the schema imported from schema.ts
+  schema,
 });
